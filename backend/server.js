1acc0fac1040d7ba1d281d165d246258f6af144f
Swaroop <swaroopcharan1712@gmail.com>
2026-06-26 19:55:53 +0530

    Added cookieparser

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./db'); 
 
 const app = express();
 connectDB();  
 
 app.use(express.json()); 
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
