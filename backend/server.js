409fab7d7d95d181d83418268835928486f76902
Swaroop <swaroopcharan1712@gmail.com>
2026-06-26 18:31:27 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 
-const app = express()
+const express = require('express');
+const connectDB = require('./db'); 
 
-app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+const app = express();
+connectDB();  
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(express.json()); 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
