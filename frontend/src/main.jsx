c9e1330bea85c707e49709f7f262a5cb1f4b2d9b
Swaroop <swaroopcharan1712@gmail.com>
2026-06-27 16:00:50 +0530

    removed old frontend, starting fresh

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'  // 👈 added this
 import './index.css'
 import App from './App.jsx'
-import Home from './Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
