4dc1f791e962b33dd71088f4cd0401e5b5ab5152
Swaroop <swaroopcharan1712@gmail.com>
2026-06-27 16:17:38 +0530

    fixed the pages routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import Home from './pages/home'
+
+function App() {
+    return (
+        <div className='App'>
+            <Routes>
+                <Route path='/' element={<Navigate to="/login" />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/home' element={<Home />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function signup(){
-    return(
+function signup() {
+    return (
         <div>signup</div>
     )
 }
