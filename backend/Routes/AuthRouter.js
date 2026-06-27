304c9f84db47601f928d7d3a1b56f9d77ba09f6b
Swaroop <swaroopcharan1712@gmail.com>
2026-06-27 15:31:13 +0530

    Added login to the router

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,17 +1,10 @@
-const { signup } = require('../Controllers/AuthController');
-const { signupValidation } = require('../Middlewares/AuthValidation');
+const { signup, login } = require('../Controllers/AuthController');
+const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
 
 const router = require('express').Router();
 
-router.post('/login',(req,res)=>{
-    res.send('login success');
-});
-
-router.post('/signup',signupValidation, signup);
-
-router.post('/signup',(req,res)=>{
-    res.send('signup success');
-});
+router.post('/login', loginValidation, login);
+router.post('/signup', signupValidation, signup);
 
 module.exports = router;
 
