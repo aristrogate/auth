52a686158a3ccfe2cc1b97467069b064800b6781
Swaroop <swaroopcharan1712@gmail.com>
2026-06-27 15:29:54 +0530

    Added jwt login logic

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,32 +1,71 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const UserModel = require("../Models/User");
 
 
-const signup = async(req,res)=>{
-    try{
-        const{username,email,password} = req.body;
-        const user = await UserModel.findOne({email});
-        if(user){
+const signup = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        if (user) {
             return res.status(409)
-            .json({message: 'User already exist, you can login', sucess:false});
+                .json({ message: 'User already exist, you can login', sucess: false });
         }
-        const userModel = new UserModel({username,email,password});
-        userModel.password = await bcrypt.hash(password,10);
+        const userModel = new UserModel({ username, email, password });
+        userModel.password = await bcrypt.hash(password, 10);
         await userModel.save();
         res.status(201)
             .json({
-                message:"Signup was a success",
-                success:true
-                })
-    } catch(err){
+                message: "Signup was a success",
+                success: true
+            })
+    } catch (err) {
         res.status(500)
-        .json({
-            message:"Internal Server Error",
-            success: false
-        })
+            .json({
+                message: "Internal Server Error",
+                success: false
+            })
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        const errorMsg = 'Auth failed, email or password is wrong';
+        if (!user) {
+            return res.status(403)
+                .json({ message: errorMsg, sucess: false });
+        }
+        const isPassEqual = await bcrypt.compare(password, user.password);
+        if (!isPassEqual) {
+            return res.status(403)
+                .json({ message: errorMsg, sucess: false });
+        }
+        const jwtToken = jwt.sign(
+            { email: user.email, _id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        )
+
+        res.status(200)
+            .json({
+                message: "Login was success",
+                success: true,
+                jwtToken,
+                email,
+                name: user.username
+            })
+    } catch (err) {
+        res.status(500)
+            .json({
+                message: "Internal Server Error",
+                success: false
+            })
     }
 }
 
 module.exports = {
-    signup
+    signup,
+    login
 }
