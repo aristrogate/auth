e739a2b5d62771b2a2b6927b2461e402efc81b96
Swaroop <swaroopcharan1712@gmail.com>
2026-06-27 15:22:20 +0530

    fixed spelling error in controller

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -11,9 +11,19 @@ const signup = async(req,res)=>{
             .json({message: 'User already exist, you can login', sucess:false});
         }
         const userModel = new UserModel({username,email,password});
-        userModel.password = await bcrypt
+        userModel.password = await bcrypt.hash(password,10);
+        await userModel.save();
+        res.status(201)
+            .json({
+                message:"Signup was a success",
+                success:true
+                })
     } catch(err){
-
+        res.status(500)
+        .json({
+            message:"Internal Server Error",
+            success: false
+        })
     }
 }
 
