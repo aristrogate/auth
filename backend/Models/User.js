9f9e376a1b2bb49b4809aca45a82f32f7f306a02
Swaroop <swaroopcharan1712@gmail.com>
2026-06-28 16:53:16 +0530

    bug fix in user.js and authvalidation.js

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,31 +1,21 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const signupValidation = (req, res, next) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).max(100).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(4).max(100).required()
-    });
-    const { error } = schema.validate(req.body);
-    if (error) {
-        return res.status(400)
-            .json({ message: "Bad request", error })
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
     }
-    next();
-}
-const loginValidation = (req, res, next) => {
-    const schema = Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().min(4).max(100).required()
-    });
-    const { error } = schema.validate(req.body);
-    if (error) {
-        return res.status(400)
-            .json({ message: "Bad request", error })
-    }
-    next();
-}
-module.exports = {
-    signupValidation,
-    loginValidation
-}
+});
+
+const UserModel = mongoose.model('users', UserSchema);
+module.exports = UserModel;
