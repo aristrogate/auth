68b5a072a6b1acc6a4dbb83ecfc2afbd3a61ba4f
Swaroop <swaroopcharan1712@gmail.com>
2026-06-28 16:04:27 +0530

    Added utils.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,37 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import { handleError, handleSuccess } from '../utils';
 
 function Signup() {
+
+    const [SignupInfo, setSignupInfo] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value);
+        const copySignupInfo = { ...SignupInfo };
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+    }
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        const { name, email, password } = SignupInfo;
+
+        if (!name || !email || !password) {
+            return handleError('Please fill out all of the fields!')
+        }
+    }
+
     return (
         <div className='container'>
             <h1>SignUp</h1>
-            <form>
+            <form onSubmit={handleSignup}>
                 <div>
-                    <label htmlFor = 'name'>Username</label>
+                    <label htmlFor='name'>Username</label>
                     <input
+                        onChange={handleChange}
                         type='text'
                         name='name'
                         autoFocus
                         placeholder="Enter your username..."
+                        value={SignupInfo.name}
                     />
                 </div>
 
                 <div>
-                    <label htmlFor = 'email'>Email</label>
+                    <label htmlFor='email'>Email</label>
                     <input
+                        onChange={handleChange}
                         type='email'
                         name='email'
                         placeholder="Enter your email..."
+                        value={SignupInfo.email}
                     />
                 </div>
 
                 <div>
-                    <label htmlFor = 'password'>Password</label>
+                    <label htmlFor='password'>Password</label>
                     <input
+                        onChange={handleChange}
                         type='password'
                         name='password'
                         placeholder="Enter your password..."
+                        value={SignupInfo.password}
                     />
                 </div>
                 <button>Sign Up</button>
